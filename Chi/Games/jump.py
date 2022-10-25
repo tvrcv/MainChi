cc9517d1d7ee79d3986102ada6efa570cccf8e75
Tomas Vargas <tomas13@vt.edu>
2022-10-25 19:34:00 +0200

    clear commit

--- Chi/Games/jump.py
+++ Chi/Games/jump.py
@@ -75,7 +75,7 @@ while True:
     background = pygame.image.load(bg1)
     background = pygame.transform.scale(background, (WIDTH, HEIGHT))
     character1 = "/Users/tovare/MainChi/Chi/Games/hariboBag.png"
-    character = pygame.image.load(character1)
+    character = pygame.image.load(character1) 
     character = pygame.transform.scale(character, (50,50))
     wood1 = "/Users/tovare/MainChi/Chi/Games/wood-.png"
     wood = pygame.image.load(wood1)
