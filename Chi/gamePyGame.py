33e31bbe53cfc20e64bda877db098df923d0768d
Tomas Vargas <tomas13@vt.edu>
2023-08-03 21:26:32 +0200

    commiting august 3rd

--- Chi/gamePyGame.py
+++ Chi/gamePyGame.py
@@ -5,29 +5,35 @@ import time
 pygame.init()
 WIDTH = 800
 HEIGHT = 600
-black=(0,0,0)
+black = (0, 0, 0)
 
-gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT))   #setting game display size
+gameDisplay = pygame.display.set_mode(
+    (WIDTH, HEIGHT))  # setting game display size
 filepath = "/Users/tovare/Chi1/Chi/Games/blueBack.jpeg"
 background = pygame.image.load(filepath)
-background = pygame.transform.scale(background, (WIDTH, HEIGHT))  #scale image 
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))  # scale image
 
-font = pygame.font.Font('comic.ttf', 40) 
+font = pygame.font.Font('comic.ttf', 40)
 
 word_speed = 0.5
 score = 0
 
+
 def new_word():
     global displayword, yourword, x_cor, y_cor, text, word_speed
-    x_cor = random.randint(300,700)     
-    y_cor = 200  #y-cor
+    x_cor = random.randint(300, 700)
+    y_cor = 200  # y-cor
     word_speed += 0.10
     yourword = ''
     words = open("words.txt").read().split(', ')
     displayword = random.choice(words)
+
+
 new_word()
 
 font_name = pygame.font.match_font('comic.ttf')
+
+
 def draw_text(display, text, size, x, y):
     font = pygame.font.Font(font_name, size)
     text_surface = font.render(text, True, black)
@@ -37,10 +43,11 @@ def draw_text(display, text, size, x, y):
 
 
 def game_front_screen():
-    gameDisplay.blit(background, (0,0))
-    if not game_over :
+    gameDisplay.blit(background, (0, 0))
+    if not game_over:
         draw_text(gameDisplay, "GAME OVER!", 90, WIDTH / 2, HEIGHT / 4)
-        draw_text(gameDisplay,"Score : " + str(score), 70, WIDTH / 2, HEIGHT /2)
+        draw_text(gameDisplay, "Score : " + str(score),
+                  70, WIDTH / 2, HEIGHT / 2)
     else:
         draw_text(gameDisplay, "Press any key to begin!", 54, WIDTH / 2, 500)
     pygame.display.flip()
@@ -65,17 +72,17 @@ while True:
     background = pygame.image.load('teacher-background.jpg')
     background = pygame.transform.scale(background, (WIDTH, HEIGHT))
     character = pygame.image.load('char.jpg')
-    character = pygame.transform.scale(character, (50,50))
+    character = pygame.transform.scale(character, (50, 50))
     wood = pygame.image.load('wood-.png')
-    wood = pygame.transform.scale(wood, (90,50))
-    gameDisplay.blit(background, (0,0))
-    gameDisplay.blit(wood,(x_cor-50,y_cor+15))
-    gameDisplay.blit(character,(x_cor-100,y_cor))
+    wood = pygame.transform.scale(wood, (90, 50))
+    gameDisplay.blit(background, (0, 0))
+    gameDisplay.blit(wood, (x_cor-50, y_cor+15))
+    gameDisplay.blit(character, (x_cor-100, y_cor))
     draw_text(gameDisplay, str(displayword), 40, x_cor, y_cor)
-    draw_text(gameDisplay, 'Score:'+str(score), 40, WIDTH/2 , 5)
+    draw_text(gameDisplay, 'Score:'+str(score), 40, WIDTH/2, 5)
 
     y_cor += word_speed
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -91,7 +98,7 @@ while True:
                 game_front_screen()
                 time.sleep(2)
                 pygame.quit()
-                
+
     if y_cor < HEIGHT-5:
         pygame.display.update()
     else:
